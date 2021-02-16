703384df8eadeaa29d79e3d8e2bd2e629fb7899b
Mauricio Gonzalez <egcoolmauricio@gmail.com>
2021-02-15 21:51:28 -0300

    Add Web API projectAdd visual Studio FIle

---
+++ RMDataManager/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RMDataManager.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            var user = RequestContext.Principal.Identity.AuthenticationType + RequestContext.Principal.Identity.Name;
+            return new string[] { "value1", "value2", user };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
