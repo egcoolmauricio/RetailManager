538bc91b671def9b457ecf957a2ca6dc554d8be0
Mauricio Gonzalez <egcoolmauricio@gmail.com>
2021-02-17 01:31:53 -0300

    Create LoginVewModel
    Add Login view and Set as Active at first

---
+++ RMWPFPresentation/ViewModels/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Caliburn.Micro;
+
+namespace RMWPFPresentation.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _password;
+        private string _userName;
+
+        public bool CanLogIn => UserName?.Length > 0 && Password?.Length > 0;
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(nameof(UserName));
+                NotifyOfPropertyChange(nameof(CanLogIn));
+            }
+        }
+
+        public void LogIn()
+        {
+        }
+    }
+}
--- RMWPFPresentation/ViewModels/ShellViewModel.cs
+++ RMWPFPresentation/ViewModels/ShellViewModel.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace RMWPFPresentation.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
-
+        private readonly LoginViewModel _loginViewModel;
+        public ShellViewModel(LoginViewModel loginViewModel)
+        {
+            _loginViewModel = loginViewModel;
+            ActivateItem(_loginViewModel);
+        }
     }
 }
